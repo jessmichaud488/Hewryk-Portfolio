17a57ceda66b00534567e59d724d5c327735bf63
Jessica <jessmichaud488@gmail.com>
2023-07-08 23:58:22 -0400

    Took out Recent Works carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,9 +1,35 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-      autoplay: true,
-      autoplaySpeed: 3000,
-      dots: true,
-      centerMode: true,
-      variableWidth: true
+/*const slides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.getElementById('prev-button');
+const nextBtn = document.getElementById('next-button');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    const images = slide.querySelectorAll('img');
+    images.forEach((image, j) => {
+      if (j === index) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
     });
   });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+nextBtn.addEventListener('click', () => {
+    nextSlide();
+});
+
+prevBtn.addEventListener('click', () => {
+    prevSlide();
+});*/
